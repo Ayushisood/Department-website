67374ca93663d1c778b9edb13278334fe33d0a34
Ayushi <soodayushi239@gmail.com>
2021-09-23 20:38:42 +0530

    updating repo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import App from "./App";
-import store from "./redux/store";
+import "./index.css";
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, 
-    document.getElementById("root")
-);
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
