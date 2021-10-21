14e59101548de72338746500e3a8210e3698e15f
Ayushi <soodayushi239@gmail.com>
2021-10-22 00:06:20 +0530

    add comest

--- src/components/Comest.js
+++ src/components/Comest.js
@@ -1,8 +1,54 @@
 import React from "react";
 import styles from "./comest.module.css";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import HorizontalCard from "./HorizontalCard";
 
 function Comest() {
-  return <div></div>;
+  return (
+    <div>
+      <Nav />
+      <div className={`${styles["comest-container"]}`}>
+        <div className={`container-fluid`}>
+          <h2 className={`${styles["comest-header"]}`}>COMEST</h2>
+          <h5 className={`${styles["comest-header-1"]}`}>
+            The Computer Society of Dyal Singh College (COMEST) aims to knit
+            together the technical, academic and socio-cultural dimensions of
+            students personality in a holistic manner. Students from all the
+            batches of B.Sc.(Hons) CS and B.Sc. Physical Science(CS) students
+            work together in teams for organizing various events throughout the
+            year which brings in dynamism that is showcased in every event.
+          </h5>
+          <div className={`row  ${styles["card-style"]}`}>
+            <HorizontalCard
+              image1="https://static.wixstatic.com/media/nsplsh_312d614132466164796463~mv2_d_5101_3401_s_4_2.jpg/v1/fill/w_575,h_438,al_c,q_80,usm_0.66_1.00_0.01/Image%20by%20NeONBRAND.webp"
+              heading1="SEMINAR & EXCURSION"
+              text1="The Department of Computer Science offers 3-year B.Sc. (Hons.) Computer Science programme through constituent colleges of the University of Delhi. The B.Sc. (Hons.) Computer Science programme primarily intends to serve as an input for higher degree academic programmes in Computer Science ."
+            />
+
+            <HorizontalCard
+              image1="https://static.wixstatic.com/media/11062b_e825515c93d94aa1af5b8bdd492155f9~mv2.jpeg/v1/crop/x_0,y_0,w_6209,h_4480/fill/w_575,h_438,al_c,q_80,usm_0.66_1.00_0.01/A%20business%20meeting%20.webp"
+              heading1="LECTURE SERIES & EVENT MANAGEMENT"
+              text1="The Department of Computer Science offers 3-year B.Sc. (Hons.) Computer Science programme through constituent colleges of the University of Delhi. The B.Sc. (Hons.) Computer Science programme primarily intends to serve as an input for higher degree academic programmes in Computer Science ."
+            />
+
+            <HorizontalCard
+              image1="https://static.wixstatic.com/media/nsplsh_3c968d5193274a80bc03c4bde13ce91e~mv2.jpg/v1/fill/w_575,h_438,al_c,q_80,usm_0.66_1.00_0.01/Image%20by%20Brett%20Jordan.webp"
+              heading1="NEWSLETTER"
+              text1="The Department of Computer Science offers 3-year B.Sc. (Hons.) Computer Science programme through constituent colleges of the University of Delhi. The B.Sc. (Hons.) Computer Science programme primarily intends to serve as an input for higher degree academic programmes in Computer Science ."
+            />
+
+            <HorizontalCard
+              image1="https://static.wixstatic.com/media/4bdf667017a541a388381135bfcea05a.jpg/v1/fill/w_575,h_438,al_c,q_80,usm_0.66_1.00_0.01/Focusing%20at%20Work.webp"
+              heading1="TECHNICAL"
+              text1="The Department of Computer Science offers 3-year B.Sc. (Hons.) Computer Science programme through constituent colleges of the University of Delhi. The B.Sc. (Hons.) Computer Science programme primarily intends to serve as an input for higher degree academic programmes in Computer Science ."
+            />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Comest;
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,83 +2,94 @@ import React from "react";
 import styles from "./gallery.module.css";
 import Nav from "./Nav";
 import Footer from "./Footer";
-import Placeholder from "react-bootstrap/Placeholder";
+//import Placeholder from "react-bootstrap/Placeholder";
 
 function Gallery() {
   return (
     <div>
       <Nav />
 
-      <Placeholder as="p" animation="glow" bg="dark">
-        <div class="container-fluid">
-          <h2 class="btn-container">Image Gallery</h2>
+      <div className={`${styles["container-size"]}`}>
+        <div className={`container-fluid`}>
+          <h2 class={`${styles["heading-style"]}`}>Gallery</h2>
           <div class="row" style={{ width: " 97.5%", margin: "auto" }}>
-            <div class="col-xs-12 col-sm-6 col-md-4 design">
+            <div class="col-xs-12 col-sm-6 col-md-4 ">
               <img
+                className={`${styles["img-style"]}`}
                 src="https://images.unsplash.com/photo-1514473776127-61e2dc1dded3?ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8M3x8fGVufDB8fHx8&ixlib=rb-1.2.1&w=1000&q=80"
                 alt="ImageOne"
                 width="100%"
               />
 
               <img
+                className={`${styles["img-style"]}`}
                 src="https://www.komando.com/wp-content/uploads/2019/09/Android-mobile-phone.jpg"
                 alt="ImageTwo"
                 width="100%"
               />
 
               <img
+                className={`${styles["img-style"]}`}
                 src="https://rxoptical.com/wp-content/uploads/Rx-Optical-Blog-Image_Dark-Mode_01.23.20.jpg"
                 alt="ImageThree"
                 width="100%"
               />
 
               <img
+                className={`${styles["img-style"]}`}
                 src="https://hocotech.com/wp-content/uploads/2018/08/shatterproof-edges-full-screen-hd-glass-a1-iphone-6-6s-plus-on-phone.jpg"
                 alt="ImageFour"
                 width="100%"
               />
             </div>
 
-            <div class="col-xs-12 col-sm-6 col-md-4 design">
+            <div class="col-xs-12 col-sm-6 col-md-4 ">
               <img
+                className={`${styles["img-style"]}`}
                 src="https://hocotech.com/wp-content/uploads/2018/08/shatterproof-edges-full-screen-hd-glass-a1-iphone-7-8-plus-on-phone-300x300.jpg"
                 alt="ImageFive"
                 width="100%"
               />
 
               <img
+                className={`${styles["img-style"]}`}
                 src="https://s3.ap-southeast-1.amazonaws.com/cdn.deccanchronicle.com/sites/default/files/iPhone7Concept%20(2)_0.jpg"
                 alt="ImageSix"
                 width="100%"
               />
 
               <img
+                className={`${styles["img-style"]}`}
                 src="http://9to5mac.com/wp-content/uploads/sites/6/2014/02/iphone-6-dumb-concept.png"
                 alt="ImageSeven"
                 width="100%"
               />
 
               <img
+                className={`${styles["img-style"]}`}
                 src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/09/13/11/iphone.jpg?width=982&height=726&auto=webp&quality=75"
                 alt="ImageEight"
                 width="100%"
               />
             </div>
 
-            <div class="col-xs-12 col-sm-6 col-md-4 design">
+            <div class="col-xs-12 col-sm-6 col-md-4 ">
               <img
+                className={`${styles["img-style"]}`}
                 src="http://9to5mac.com/wp-content/uploads/sites/6/2014/02/iphone-6-dumb-concept.png"
                 alt="ImageNine"
                 width="100%"
               />
 
               <img
+                className={`${styles["img-style"]}`}
                 src="https://hocotech.com/wp-content/uploads/2018/08/shatterproof-edges-full-screen-hd-glass-a1-iphone-7-8-plus-on-phone-300x300.jpg"
                 alt="ImageTen"
                 width="100%"
               />
 
               <img
+                className={`${styles["img-style"]}`}
                 src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/09/13/11/iphone.jpg?width=982&height=726&auto=webp&quality=75"
                 alt="ImageEleven"
                 width="100%"
@@ -86,7 +97,7 @@ function Gallery() {
             </div>
           </div>
         </div>
-      </Placeholder>
+      </div>
 
       <Footer />
     </div>
