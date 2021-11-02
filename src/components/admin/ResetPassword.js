f8a461d613e8bc98c0a2375b2d31c2859de1bd59
Rachit Mittal <masterrachit777@gmail.com>
2021-11-02 20:58:43 +0530

    update reset password

--- src/components/admin/ResetPassword.js
+++ src/components/admin/ResetPassword.js
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import styles from "./signup.module.css";
 import Form from "react-bootstrap/Form";
 import axios from "axios";
-//import { Redirect, Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 
 function ResetPassword() {
-  const [responseData, setResponseData] = useState();
+  const [linkSent, setLinkSent] = useState(false);
+
   const handleResetPassword = async (event) => {
     event.preventDefault();
     const postBody = new URLSearchParams({
@@ -14,7 +15,7 @@ function ResetPassword() {
     }).toString();
 
     const response = await axios.post(
-      "http://localhost:5000/api/resetPassword",
+      "http://localhost:5000/api/reset-password",
       postBody,
       {
         withCredentials: true,
@@ -23,35 +24,47 @@ function ResetPassword() {
         },
       }
     );
-    setResponseData(response.data);
+    if(response.data.linkSent){
+      alert(response.data.msg);
+      setLinkSent(true);
+    } else{
+      alert(response.data.msg)
+    }
   };
-  return (
-    <div className={`${styles["signUp"]}`}>
-      <Form
-        className={`${styles["signUp-container"]} container-fluid`}
-        onSubmit={(event) => handleResetPassword(event)}
-      >
-        <Form.Text className={`${styles["h1-style"]} text-muted`}>
-          <h1>Reset your password</h1>
-        </Form.Text>
 
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label className={`${styles["label-style"]}`}>
-            Registered Email address
-          </Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter your registered email"
-            name="username"
-          />
-        </Form.Group>
+  if(linkSent){
+    return (
+      <Redirect to='/admin/signin' />
+    )
+  } else {
+    return (
+      <div className={`${styles["signUp"]}`}>
+        <Form
+          className={`${styles["signUp-container"]} container-fluid`}
+          onSubmit={(event) => handleResetPassword(event)}
+        >
+          <Form.Text className={`${styles["h1-style"]} text-muted`}>
+            <h1>Reset your password</h1>
+          </Form.Text>
 
-        <Button variant="primary" type="submit">
-          Send confirmation link
-        </Button>
-      </Form>
-    </div>
-  );
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label className={`${styles["label-style"]}`}>
+              Registered Email address
+            </Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter your registered email"
+              name="username"
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Send confirmation link
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
 
 export default ResetPassword;
