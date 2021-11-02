3c4877ba01c25bdd5e9c933c4f07de0a89659775
Ayushi <soodayushi239@gmail.com>
2021-11-02 14:07:46 +0530

    update reset-password

--- src/components/admin/ResetPassword.js
+++ src/components/admin/ResetPassword.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./signup.module.css";
 import Form from "react-bootstrap/Form";
+import axios from "axios";
 //import { Redirect, Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 
 function ResetPassword() {
-  function handleResetPassword() {}
+  const [responseData, setResponseData] = useState();
+  const handleResetPassword = async (event) => {
+    event.preventDefault();
+    const postBody = new URLSearchParams({
+      username: event.target.username,
+    }).toString();
+
+    const response = await axios.post(
+      "http://localhost:5000/api/resetPassword",
+      postBody,
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    setResponseData(response.data);
+  };
   return (
     <div className={`${styles["signUp"]}`}>
       <Form
